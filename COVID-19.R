f5212a29c7a4249149119728b28f72f78eb5324e
Sören Thiering <soeth16@gmail.com>
2020-07-29 00:09:19 +0200

    Update Data 07/29/2020 00:09

--- COVID-19.R
+++ COVID-19.R
@@ -206,7 +206,9 @@ JuliaCall::julia_eval("@everywhere phi_d_1 = $phi_d_1")
 JuliaCall::julia_eval("@everywhere phi_d_2 = $phi_d_2")
 
 JuliaCall::julia_eval("@everywhere function k(p, t)
-    if (t >= p[12]) 
+    if (t >= p[14]) 
+      p[15]
+    elseif (t >= p[12] && t < p[14]) 
       p[13]
     elseif (t >= p[10] && t < p[12]) 
       p[11]
@@ -399,7 +401,7 @@ JuliaCall::julia_eval("obj = build_loss_objective(prob, Rodas5(), reltol=1e-4, a
 
 
 
-JuliaCall::julia_eval("bound1 = Tuple{Float64,Float64}[(0.25,0.35),(11.6,11.8),(0.2,0.3),(21.8,22),(0.15,0.2),(52,54),(0.15,0.2),(61,63),(0.15,0.2),(100,103),(0.15,0.3),(110,115),(0.15,0.25)]")
+JuliaCall::julia_eval("bound1 = Tuple{Float64,Float64}[(0.25,0.35),(11.6,11.8),(0.2,0.3),(21.8,22),(0.15,0.2),(52,54),(0.15,0.2),(61,63),(0.15,0.2),(100,103),(0.15,0.3),(110,115),(0.15,0.25),(125,135),(0.15,0.25)]")
 JuliaCall::julia_eval("res1 = bboptimize(obj;SearchRange = bound1, MaxSteps = 11e3, NumDimensions = 15,
     Workers = workers(),
     TraceMode = :compact,
